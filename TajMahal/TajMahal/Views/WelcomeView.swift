33dd00eb9615ec65f5b9b9515da4c2e1f7a9a322
Altieri <steven_altieri@yahoo.com>
2024-06-19 15:18:39 +0200

    first push

--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -16,7 +16,7 @@ struct WelcomeView: View {
             NavigationLink {
                 MenuView()
             } label : {
-                Text("Menu")
+                Text("Menggffg")
             }
         }
     }
